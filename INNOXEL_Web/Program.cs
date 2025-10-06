c404d6841a70b36b399737ef0d69049b41f1fba6
Jan Ellenberger <j.ellenberger@zidatech.ch>
2025-10-06 13:56:52 +0200

    darkmode

--- INNOXEL_Web/Program.cs
+++ INNOXEL_Web/Program.cs
@@ -9,5 +9,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddMudServices();
+builder.Services.AddMudBlazorDialog();
+builder.Services.AddMudBlazorSnackbar();
+builder.Services.AddMudBlazorResizeListener();
 
 await builder.Build().RunAsync();
